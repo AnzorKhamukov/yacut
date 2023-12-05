ff178c09d263a8d90a042c67b5bb9ab2db57a6bb
Anzor Khamukov <AnzorKhamukov@yandex.ru>
2023-12-05 22:23:30 +0300

    fz

---
+++ settings.py
@@ -0,0 +1,14 @@
+import os
+import secrets
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI',
+        default='sqlite:///db.sqlite3'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv(
+        'SECRET_KEY',
+        default=secrets.token_urlsafe(16)
+    )
